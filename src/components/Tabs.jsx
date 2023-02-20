1869db680723ca224745f6bf50afd3946ae74812
ammo <ammo@noobuntu.lan>
2023-02-20 17:49:47 +0630

    back to home on tab change

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -1,15 +1,21 @@
 import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { HiOutlineFire, HiOutlineTrendingUp, HiOutlineSparkles } from 'react-icons/hi';
 
 import { StoryContext } from '../hooks/story-hook';
 
 export default function Tabs() {
-
     const { tab, setTab } = useContext(StoryContext);
+    const navigate = useNavigate();
+    const handleTabChange = (tab) => () => {
+        setTab(tab);
+        navigate('..');
+    }
+
     return (
         <nav className='flex items-center justify-center gap-4'>
             <abbr title='Top Stories'>
-                <button onClick={() => setTab('topstories')}>
+                <button onClick={handleTabChange('topstories')}>
                     <HiOutlineFire
                         className={`w-6 h-6 cursor-pointer hover:text-red-500 hover:scale-110 transition ${tab === 'topstories' && 'text-red-500'}`}
                     />
@@ -17,7 +23,7 @@ export default function Tabs() {
             </abbr>
 
             <abbr title='New Stories'>
-                <button onClick={() => setTab('newstories')}>
+                <button onClick={handleTabChange('newstories')}>
                     <HiOutlineSparkles
                         className={`w-6 h-6 cursor-pointer hover:text-yellow-500 hover:scale-110 transition ${tab === 'newstories' && 'text-yellow-500'}`}
                     />
@@ -25,7 +31,7 @@ export default function Tabs() {
             </abbr>
 
             <abbr title='Best Stories'>
-                <button onClick={() => setTab('beststories')}>
+                <button onClick={handleTabChange('beststories')}>
                     <HiOutlineTrendingUp
                         className={`w-6 h-6 cursor-pointer hover:text-green-500 hover:scale-110 transition ${tab === 'beststories' && 'text-green-500'}`}
                     />
