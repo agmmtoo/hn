783e0c3f2ac97de1163f157032e8546ce7946010
ammo <solidifyarmor@gmail.com>
2023-05-12 23:12:41 +0630

    search link

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
-import React, { Suspense } from 'react';
-import { Routes, Route } from 'react-router-dom';
+import React, { Suspense } from "react";
+import { Routes, Route } from "react-router-dom";
 
 // pages
-import Home from './pages/Home';
-import Stories from './pages/Stories';
-const Story = React.lazy(() => import('./pages/Story'));
-const Bookmarks = React.lazy(() => import('./pages/Bookmarks'));
+import Home from "./pages/Home";
+import Stories from "./pages/Stories";
+
+const Story = React.lazy(() => import("./pages/Story"));
+const Bookmarks = React.lazy(() => import("./pages/Bookmarks"));
+const Search = React.lazy(() => import("./pages/Search"));
 
 function App() {
   return (
-    <Suspense fallback={<div className='w-full h-full bg-emerald-400' />}>
+    <Suspense fallback={<div className="w-full h-full bg-emerald-400" />}>
       <Routes>
-        <Route path='/' element={<Home />}>
-          <Route path=':id' element={<Story />} />
-          <Route path='bookmarks' element={<Bookmarks />} />
+        <Route path="/" element={<Home />}>
+          <Route path=":id" element={<Story />} />
+          <Route path="bookmarks" element={<Bookmarks />} />
+          <Route path="search" element={<Search />} />
           <Route index element={<Stories />} />
         </Route>
       </Routes>
