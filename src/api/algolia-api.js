4427575297aac77d27489521a5db30829cb52426
ammo <solidifyarmor@gmail.com>
2023-09-16 09:45:58 +0630

    visible scroll to top bar

--- src/api/algolia-api.js
+++ src/api/algolia-api.js
@@ -4,4 +4,52 @@ const algoaxios = axios.create({
     baseURL: 'http://hn.algolia.com/api/v1/',
 })
 
-export async function fetchStories(tab) {
+export async function search(query, token) {
+    const response = await algoaxios.get('search', {
+        params: {
+            query
+        },
+        signal: token
+    })
+    return response.data.hits
+}
+
+// {
+//     "created_at": "2018-03-14T03:50:30.000Z",
+//     "title": "Stephen Hawking has died",
+//     "url": "http://www.bbc.com/news/uk-43396008",
+//     "author": "Cogito",
+//     "points": 6015,
+//     "story_text": null,
+//     "comment_text": null,
+//     "num_comments": 436,
+//     "story_id": null,
+//     "story_title": null,
+//     "story_url": null,
+//     "parent_id": null,
+//     "created_at_i": 1520999430,
+//     "relevancy_score": 8012,
+//     "_tags": [
+//         "story",
+//         "author_Cogito",
+//         "story_16582136"
+//     ],
+//     "objectID": "16582136",
+//     "_highlightResult": {
+//         "title": {
+//             "value": "Stephen Hawking has died",
+//             "matchLevel": "none",
+//             "matchedWords": []
+//         },
+//         "url": {
+//             "value": "http://www.bbc.com/news/uk-43396008",
+//             "matchLevel": "none",
+//             "matchedWords": []
+//         },
+//         "author": {
+//             "value": "Cogito",
+//             "matchLevel": "none",
+//             "matchedWords": []
+//         }
+//     }
+// }
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,17 +1,29 @@
-import { useSearchParams } from "react-router-dom"
+import { useEffect } from "react";
+import { useSearchParams } from "react-router-dom";
+
+import { search } from "../api/algolia-api";
 
 export default function Search() {
-    const [searchParams, setSearchParams] = useSearchParams()
-    const query = searchParams.get("query")
+  const [searchParams, setSearchParams] = useSearchParams({ query: '' });
+  const query = searchParams.get("query");
+  const handleSearchChange = (event) => {
+    setSearchParams((param) => ({ ...param, query: event.target.value }));
+  };
 
-    console.log(searchParams)
-    const handleSearchChange = (event) => {
-        console.log(event.target.value)
-        setSearchParams((param) => ({ ...param, query: event.target.value }))
-    }
-    return (
-        <>
-            <input onChange={handleSearchChange} value={query} className='w-full p-2 border-sky-300 bg-slate-100 dark:bg-zinc-800' />
-        </>
-    )
+  useEffect(() => {
+    const abortController = new AbortController();
+    const signal = abortController.signal;
+
+    search(query, signal).then(console.log).catch(console.error);
+    return () => abortController.abort();
+  }, [query]);
+  return (
+    <>
+      <input
+        onChange={handleSearchChange}
+        value={query}
+        className="w-full p-2 border-sky-300 bg-slate-100 dark:bg-zinc-800"
+      />
+    </>
+  );
 }
