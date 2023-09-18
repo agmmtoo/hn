42602d59c04f16cb7089e2dc0b7b5e38a499d1a7
Aung Myint Myat Oo <aungmyintmyatoo@yomafleet.com>
2023-09-18 15:35:00 +0630

    fix: action bar color

--- src/components/ActionBar.jsx
+++ src/components/ActionBar.jsx
@@ -3,7 +3,7 @@ export default function ActionBar() {
     // scroll to top on click
     return (
         <div
-            className='text-transparent hover:text-white text-center sticky top-0 cursor-pointer hover:bg-sky-400 dark:hover:bg-sky-800 ease-out duration-150 hover:transition-colors hover:ease-in'
+            className='text-transparent hover:text-white text-center sticky top-0 cursor-pointer hover:bg-sky-800 dark:hover:bg-sky-400 ease-out duration-150 hover:transition-colors hover:ease-in'
             onClick={() => window.scrollTo(0, 0)}
         >Scroll to Top</div>
     );
